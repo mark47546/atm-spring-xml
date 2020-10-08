5121cc0a9c00ddd68f8d6b347f05ae49a35e225b
mark47546 <mark47546natthaphon@gmail.com>
2020-10-08 15:05:00 +0700

    done

---
+++ src/main/java/atm/config/ATMConfig.java
@@ -0,0 +1,25 @@
+package atm;
+
+/**
+ * An ATM that accesses a bank.
+ */
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceDB();
+    }
+
+    @Bean
+    public Bank bank() {
+        return new Bank("My Bank", dataSource());
+    }
+
+    @Bean
+    public ATM atm() {
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI() {
+        return n
