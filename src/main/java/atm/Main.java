566b328703b603b1656232d4195d9f90c52500ad
mark47546 <mark47546natthaphon@gmail.com>
2020-10-08 15:21:04 +0700

    done

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,13 +1,13 @@
 package atm;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
-                new AnnotationConfigApplicationContext(ATMConfig.class);
+                new ClassPathXmlApplicationContext("bean.xml");
 
-        AtmUI atmUI = context.getBean(AtmUI.class);
-        atmUI.run();
+        AtmUI atmUI= context.getBean(“atmUI", AtmUI.class);
+                atmUI.run();
     }
 }
